be6658172b363b1277dc2eb49981c1e4dc430580
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-02-22 18:36:47 +0300

    init: start transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,44 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-};
+  
+module.exports = function transform(arr) {
+    let array = [];
+    let check = 0;
+
+      if(Array.isArray(arr)) {
+        if (arr.length == 0 ) {
+            return arr;
+            
+        } else {
+          for (let i = 0; i < arr.length; i++){
+              if (arr[i] === '--discard-next' ) {
+                  check = 1;
+                  
+              } else if (arr[i] === '--discard-prev') {
+                if (i > 0) {
+                    array.pop();
+                    
+                };
+              } else if (arr[i] === '--double-next') {
+                if (i < arr.length - 1) {
+                    array.push(arr[i + 1]);
+                    
+                };
+              } else if (arr[i] === '--double-prev') {
+                if (i > 0) {
+                    array.push(arr[i - 1]);
+                    
+                };
+              } else if (check == 1) {
+                 check = 0;
+                  
+              } else {
+                  array.push(arr[i]);
+                  
+              };
+          };
+        } 
+      } else {
+          throw new Error("Error");
+    }
+    
+      return array;
+  };
