d7db95f51effc7deaec5ad4bc355d4e3838c887d
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-02-22 13:28:56 +0300

    init: start count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,7 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  const flat = matrix.flat(2);
+  return flat.reduce((totall, el) => {
+    if (el === "^^")++totall;
+    return totall;
+  }, 0)
 };
