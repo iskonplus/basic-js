e09b3785ec45a50fcca4fef68b0a2e756d771dc0
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-03-07 21:12:16 +0300

    init: start dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,16 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(arr) {
+  let nameItem = ""
+  if (!Array.isArray(arr)) {
+      return false
+    }
+
+  for (let i = 0; i < arr.length; i++) {
+     
+      typeof arr[i] !== "string" ||
+        (nameItem += arr[i].trim()[0]);
+    
+  } 
+
+ return nameItem.toUpperCase().split("").sort().join("")
+
 };
