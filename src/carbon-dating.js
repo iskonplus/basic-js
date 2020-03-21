15264833d1e28024677123586ac7eb5f5e1a1df7
iskonplus <58971909+iskonplus@users.noreply.github.com>
2020-03-21 23:22:36 +0300

    init: adding a task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,19 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  if (typeof sampleActivity !== "string"
+    
+    || parseInt(sampleActivity) > 15
+    || parseInt(sampleActivity) <= 0
+    || isNaN(parseInt(sampleActivity))
+    
+  ) {
+    
+    return false;
+  }
+
+  return Math.ceil((Math.log(MODERN_ACTIVITY / parseInt(sampleActivity)) * HALF_LIFE_PERIOD) / 0.693);
+
 };
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,27 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        let result = 0;
+        if (typeof arr === "object") {
+            result += 1
+        }
+    
+        arr.forEach(el => {
+            if (typeof el === "object") {
+                result += 1;
+                check(el)
+            } 
+        });
+    
+        function check(el) {
+            el.forEach(el => {
+                if (typeof el === "object") {
+                    result += 1;
+                    check(el)
+                } 
+            })
+        }
+    
+        return result
+
     }
 };
